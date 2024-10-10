7ea708158f0df1e65e146c8609964dccd854ceb6
Szymon Bartkowiak <szymon.b4310@gmail.com>
2024-10-10 09:35:49 +0200

    Dodanie object pola

--- src/main/java/org/example/Singelton/zadanie2/ConnectionManagerImpl.java
+++ src/main/java/org/example/Singelton/zadanie2/ConnectionManagerImpl.java
@@ -1,10 +1,18 @@
 package org.example.Singelton.zadanie2;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class ConnectionManagerImpl implements ConnectionManager {
     private static ConnectionManager instance;
     private static final int MAX_POOL_SIZE = 3;
-    private int objectCount = 0;
+    private Map<String, List<DatabaseConnection>> connectionPools = new HashMap<>();
+    private Map<String, Integer> connectionIndexes = new HashMap<>();
+
     private ConnectionManagerImpl() {
+
     }
 
     public static ConnectionManager getInstance() {
@@ -16,8 +24,23 @@ public class ConnectionManagerImpl implements ConnectionManager {
 
     @Override
     public DatabaseConnection getConnection(String databaseName) {
-        objectCount++;
-        Database database = Database.getInstance(databaseName);
-        return database.getConnection();
+        if (!connectionPools.containsKey(databaseName)) {
+            connectionPools.put(databaseName, new ArrayList<>());
+            connectionIndexes.put(databaseName, 0);
+        }
+
+        List<DatabaseConnection> pool = connectionPools.get(databaseName);
+        int index = connectionIndexes.get(databaseName);
+
+
+        if (pool.size() < MAX_POOL_SIZE) {
+            DatabaseConnection connection = Database.getInstance(databaseName).getConnection();
+            pool.add(connection);
+        }
+
+        DatabaseConnection currentConnection = pool.get(index);
+        connectionIndexes.put(databaseName, (index + 1) % MAX_POOL_SIZE);
+
+        return currentConnection;
     }
 }
--- src/main/java/org/example/Singelton/zadanie2/Ztp01.java
+++ src/main/java/org/example/Singelton/zadanie2/Ztp01.java
@@ -6,11 +6,33 @@ public class Ztp01 {
         ConnectionManager connectionManager = ConnectionManagerImpl.getInstance();
 
         DatabaseConnection connection1 = connectionManager.getConnection("DB1");
+        DatabaseConnection connection2 = connectionManager.getConnection("DB1");
+        DatabaseConnection connection3 = connectionManager.getConnection("DB1");
+        DatabaseConnection connection4 = connectionManager.getConnection("DB1");
 
-        if (connection1 != null) {
-            connection1.addRecord("Karol", 23);
-            connection1.showAllRecords();
+
+        connection1.addRecord("Karol", 23);
+        connection2.addRecord("Karol1", 23);
+        connection3.addRecord("Karol2", 23);
+        connection4.addRecord("Karol3", 23);
+
+
+        System.out.println("C1:");
+        connection1.showAllRecords();
+        System.out.println("C2:");
+        connection2.showAllRecords();
+        System.out.println("C3:");
+        connection3.showAllRecords();
+        System.out.println("C4:");
+        connection4.showAllRecords();
+
+        if (connection1.hashCode() == connection3.hashCode()) {
+            System.out.println("connection1 i connection4 to ten sam obiekt.");
+        } else {
+            System.out.println("connection1 i connection4 to różne obiekty.");
         }
+
+
 
 
     }
